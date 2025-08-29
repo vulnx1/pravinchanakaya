75aeabdd4821e008716acf62461b2273335a60ed
vulnx1 <145221347+vulnx1@users.noreply.github.com>
2025-08-29 18:31:41 +0530

    main_commit

--- build/src/config/emailjs.ts
+++
@@ -1,5 +0,0 @@
-export const emailjsConfig = {
-  serviceId: 'service_g1lcr9h',
-  templateId: 'template_hm7n01g', // Replace with your actual template ID
-  publicKey: 'WlDNnbV0L2JOyiz34', // Replace with your EmailJS public key
-};
